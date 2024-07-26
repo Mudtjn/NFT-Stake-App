8e2c278fdc4dd8c68dd7b58adc1163fe8e72ac61
Sigma2345 <95545310+Sigma2345@users.noreply.github.com>
2024-07-26 23:28:59 +0530

    initial commit

---
+++ src/NftStakeContract.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier:MIT
+pragma solidity ^0.8.20; 
+
+/**
+ * @title NftStakeContract
+ * @author Mudit Jain
+ * @notice 
+ */
+contract NftStakeContract {
+
+}
---
+++ src/StakeToken.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier:MIT
+pragma solidity ^0.8.20; 
+
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol"; 
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol"; 
+
+contract StakeToken is ERC20, Ownable {
+
+    error StakeToken__NotZeroAddress(); 
+    error StakeToken__MustBeMoreThanZero(); 
+
+    constructor() ERC20("StakeToken", "StT") Ownable(msg.sender) {}
+
+    function mint(address _to, uint256 _amount) external onlyOwner returns(bool){
+        if(address(_to) == address(0)) revert StakeToken__NotZeroAddress(); 
+        if(_amount == 0) revert StakeToken__MustBeMoreThanZero(); 
+        _mint(_to, _amount); 
+        return true;
+    }
+} 
